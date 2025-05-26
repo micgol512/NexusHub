9d2794ace64569a218e78999bce4b9c422a4fc2b
Michał Gołębiowski <m.golebiowski.fm@gmail.com>
2025-05-26 13:31:18 +0200

    FIX and FEAT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col w-full h-full gap-[100px] max-w-[1920px]">
       <CategorySlider />
-      {/* <RecomendationSlider /> */}
+      <RecomendationSlider />
       <BrandSlider />
     </div>
   );
